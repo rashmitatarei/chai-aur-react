5a3bf17c6cb77299ae1a56930a1b7840aec52523
Naveen Mohanty <naveenmohanty1234@gmail.com>
2024-03-05 21:06:35 +0530

    corrected the code

--- 05passworgenerator/src/App.jsx
+++ 05passworgenerator/src/App.jsx
@@ -1,88 +1,86 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react';
 
-import './App.css'
+import './App.css';
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
 
-  const [charAllwed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("")
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState('');
+
   const passwordGenerator = useCallback(() => {
-    let pass = ""
-    let str = ""
-    "ABCDEFGHIJKLMNOPQRSHTUVWXYZabcdefghijklmnopqrstvuwxyz"
-    if (numberAllowed) str += "0123456789"
-    if (password) str += "!@$%&*-=+{}[]~`"
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (numberAllowed) str += '0123456789';
+    if (charAllowed) str += '!@#$%^&*-_+=[]{}~`';
 
-    for (let i = 1; i <= arr.length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
-
-      pass = str.charAt(char)
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
-    setPassword(pass)
 
-
-  }, [length, numberAllowed, charAllwed, setPassword])
-
-
-
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
   return (
     <>
-      <div className="w-full max-w-md mx-auto shaow-md rounded-lg  px-4 my-8 text-orange-500 bg-gray-700" >
+      <div className='w-full max-w-md mx-auto shaow-md rounded-lg  px-4 my-8 text-orange-500 bg-gray-700'>
         <h1 className='text-white text-center'>passwordGenerator</h1>
 
-        <div className='className=" flex shadow rounded-lg overflow-hidden mb-4" ' >
+        <div className='className=" flex shadow rounded-lg overflow-hidden mb-4" '>
           <input
-            type="text"
+            type='text'
             value={password}
             className='outline-none w-full py-1 px-3'
             placeholder='password '
             readOnly
           />
-          <button className='bg-blue-700 text-white outline-none px-3 py-0.5 sharink-0'>copy</button>
-
+          <button className='bg-blue-700 text-white outline-none px-3 py-0.5 sharink-0'>
+            copy
+          </button>
         </div>
         <div className='flex text-5m gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input
-              type="range"
+              type='range'
               min={6}
               max={100}
               value={length}
               className='cursor-pointer'
-              onChange={(e) => { setLength(e.target.value) }}
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
             />
             <label>Length:{length}</label>
           </div>
         </div>
         <div className='flex items-center gap-x-1'>
           <input
-            type="checkbox"
+            type='checkbox'
             defaultChecked={numberAllowed}
             id='numberInput'
             onChange={() => {
               setNumberAllowed((prev) => !prev);
             }}
           />
-          <label htmlfor="numberInput">Numbers</label>
+          <label htmlFor='numberInput'>Numbers</label>
         </div>
         <div className='flex items-center gap-x-1'>
           <input
-            type="checkbox"
-            defaultChecked={charAllwed}
+            type='checkbox'
+            defaultChecked={charAllowed}
             id='characterInput'
             onChange={() => {
               setCharAllowed((prev) => !prev);
             }}
-
           />
-          <label htmlfor="charterInput"> Character</label>
+          <label htmlFor='charterInput'> Character</label>
         </div>
       </div>
-
     </>
-  )
-
+  );
 }
 export default App;
